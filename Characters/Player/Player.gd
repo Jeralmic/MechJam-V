3047fe74f0c6084001843dcb4fce1b14955ee431
BK0N <168575891+Jeralmic@users.noreply.github.com>
2024-07-15 18:47:34 -0400

    Added camera

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -7,5 +7,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta: float) -> void:
+	
+	var jump = Vector2.ZERO
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.z = Input.get_axis("ui_up","ui_down")
+	
+	apply_central_force(input * 1200.0 * delta)
+	
